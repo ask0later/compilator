db0f9ed42865d17f24da8929a38810f6bfe67b19
Yaroslav Kuznetsov <asklater@bk.ru>
2024-05-07 19:16:02 +0300

    add example program

--- back_end/nasm_file.asm
+++ back_end/nasm_file.asm
@@ -8,14 +8,14 @@ nop
  func_1 :
 call my_input 
 push rax 
-pop  QWORD [r10 + 8] 
-push  QWORD [r10 + 8] 
-add r10 , 24 
-pop  QWORD [r10 + 0] 
-call func_0 
-sub r10 , 24 
 pop  QWORD [r10 + 16] 
 push  QWORD [r10 + 16] 
+add r10 , 32 
+pop  QWORD [r10 + 0] 
+call func_0 
+sub r10 , 32 
+pop  QWORD [r10 + 24] 
+push  QWORD [r10 + 24] 
 pop rax 
 call my_output 
 push 60 
@@ -26,47 +26,32 @@ syscall
 nop 
  func_0 :
 push  QWORD [r10 + 0] 
-push 0 
-pop r11 
-pop r12 
-cmp r11 , r12 
-jne .end_if_0 
-push 1 
-pop r8 
-pop r9 
-push r8 
-push r9 
-ret 
- .end_if_0 :
+pop  QWORD [r10 + 8] 
+ .while_0 :
 push  QWORD [r10 + 0] 
 push 1 
 pop r11 
 pop r12 
 cmp r11 , r12 
-jne .end_if_1 
-push 1 
-pop r8 
-pop r9 
-push r8 
-push r9 
-ret 
- .end_if_1 :
-push  QWORD [r10 + 0] 
+je .end_while_0 
 push  QWORD [r10 + 0] 
 push 1 
 pop r12 
 pop r11 
 sub r11 , r12 
 push r11 
-add r10 , 8 
 pop  QWORD [r10 + 0] 
-call func_0 
-sub r10 , 8 
+push  QWORD [r10 + 0] 
+push  QWORD [r10 + 8] 
 pop r12 
 pop r11 
 mov rax , r11 
 mul r12 
 push rax 
+pop  QWORD [r10 + 8] 
+jmp .while_0 
+ .end_while_0 :
+push  QWORD [r10 + 8] 
 pop r8 
 pop r9 
 push r8 
